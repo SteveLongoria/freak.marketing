d34021210e94cf7b3f8630d342fe3326c3179dd8
Steve Longoria <stevel@hey.com>
2025-07-02 23:58:16 -0700

    Homepage finished

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import ButtonLogin from "./components/ButtonLogin";
 import FAQListItem from "./components/FAQListItem";
+import Image from "next/image";
+import productDemo from "./productDemo.jpeg";
 
 export default function Home() {
   const isLoggedIn = true;
@@ -9,9 +11,9 @@ export default function Home() {
     <main>
       {/* HEADER */}
       <section className="bg-base-200">
-        <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+        <div className="max-w-5xl mx-auto flex justify-between items-center px-8 py-2">
           <div className="font-bold">Freak.Marketing</div>
-          <div className="space-x-4">
+          <div className="space-x-4 hidden lg:block">
             <a className="link link-hover" href="#pricing">
               Pricing
             </a>
@@ -25,16 +27,24 @@ export default function Home() {
         </div>
       </section>
       {/* HERO */}
-      <section className="text-center py-32 px-8 max-w-3xl mx-auto">
-        <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
-          Collect customer feedback to build better products
-        </h1>
-        <div className="opacity-90 mb-10">
-          Create a feedback board in minutes, prioritize features and build
-          products your customers will love.
-        </div>
+      <section className="text-center lg:text-left py-32 px-8 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start">
+        <Image
+          src={productDemo}
+          alt="Product Demo"
+          className="w-96 rounded-xl"
+        />
 
-        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+        <div>
+          <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
+            Collect customer feedback to build better products
+          </h1>
+          <div className="opacity-90 mb-10">
+            Create a feedback board in minutes, prioritize features and build
+            products your customers will love.
+          </div>
+
+          <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+        </div>
       </section>
 
       {/* PRICING */}
